7eb2553065f55fb524b07619c07018832e6fadac
Tyers <matt.tyers@dfg.alaska.local>
2023-10-05 16:17:25 -0800

    refining tabled output, adding KING ASL pooled by subdistrict

--- R/Chitina_sampling_2019_2022.R
+++ R/Chitina_sampling_2019_2022.R
@@ -18,13 +18,13 @@ GSD <- list()   # GSD harvest reported from all years
 CSD <- list()   # CSD harvest reported from all years
 
 for(iyear in seq_along(years)) {
-  sockeye[[iyear]] <- read.csv(file=paste0(years[iyear], " Raw Data - Sockeye.csv")) %>% 
+  sockeye[[iyear]] <- read.csv(file=paste0("Data/", years[iyear], " Raw Data - Sockeye.csv")) %>% 
     clean_names %>% remove_empty(which="cols")
-  chinook[[iyear]] <- read.csv(file=paste0(years[iyear], " Raw Data - Chinook.csv")) %>% 
+  chinook[[iyear]] <- read.csv(file=paste0("Data/", years[iyear], " Raw Data - Chinook.csv")) %>% 
     clean_names %>% remove_empty(which="cols")
-  GSD[[iyear]] <- read.csv(file=paste0(years[iyear], " GSD HARVEST Reported.csv")) %>% 
+  GSD[[iyear]] <- read.csv(file=paste0("Data/", years[iyear], " GSD HARVEST Reported.csv")) %>% 
     clean_names %>% remove_empty(which="cols")
-  CSD[[iyear]] <- read.csv(file=paste0(years[iyear], " CSD HARVEST Reported.csv")) %>% 
+  CSD[[iyear]] <- read.csv(file=paste0("Data/", years[iyear], " CSD HARVEST Reported.csv")) %>% 
     clean_names %>% remove_empty(which="cols")
 }
 
@@ -165,11 +165,13 @@ C_hi_CSD <- o_hi_CSD/n_i_CSD*N_CSD
 # equation 2
 C_sh_GSD <- colSums(C_hi_GSD, na.rm=T)
 C_sh_CSD <- colSums(C_hi_CSD, na.rm=T)
+# NOTE: I don't think this is appropriate for 2022 GSD, given the missing data
 
 # for table
 p_GSD <- o_hi_GSD/n_i_GSD
 p_CSD <- o_hi_CSD/n_i_CSD
-# should plot this, I think it would be cool
+vp_GSD <- p_GSD*(1-p_GSD)/(n_i_GSD-1)
+vp_CSD <- p_CSD*(1-p_CSD)/(n_i_CSD-1)
 
 # making a quick exploratory plot of the proportions
 par(mfrow=c(1,2))
@@ -202,7 +204,32 @@ SE_C_h_GSD <- sqrt(V_C_h_GSD)
 SE_C_hi_CSD <- sqrt(V_C_hi_CSD)
 SE_C_h_CSD <- sqrt(V_C_h_CSD)
 
-paste0(C_hi_GSD, " (", SE_C_hi_GSD, ")")
+printse <- function(est, se, percent=F, digits=2) {
+  if(percent) {
+    out <- paste0(round(100*est, digits), "% (", round(100*se, digits),"%)")
+  } else {
+    out <- paste0(round(est, digits), " (", round(se, digits),")")
+  }
+  return(out)
+}
+
+# making tables that mimic the structure of T5 and T6 in report
+T5_update <- data.frame(1:nrow(n_i_GSD))
+for(i in seq_along(years)) {
+  T5_update[, 3*(i-1)+1] <- n_i_GSD[,i]
+  T5_update[, 3*(i-1)+2] <- printse(p_GSD[,i], sqrt(vp_GSD[,i]), percent=T, digits=1)
+  T5_update[, 3*(i-1)+3] <- printse(C_hi_GSD[,i], SE_C_hi_GSD[,i], digits=0)
+}
+names(T5_update) <- paste(rep(years,each=3), rep(c("n","p","c")))
+
+T6_update <- data.frame(1:nrow(n_i_CSD))
+for(i in seq_along(years)) {
+  T6_update[, 3*(i-1)+1] <- n_i_CSD[,i]
+  T6_update[, 3*(i-1)+2] <- printse(p_CSD[,i], sqrt(vp_CSD[,i]), percent=T, digits=1)
+  T6_update[, 3*(i-1)+3] <- printse(C_hi_CSD[,i], SE_C_hi_CSD[,i], digits=0)
+}
+names(T6_update) <- paste(rep(years,each=3), rep(c("n","p","c")))
+
 
 
 
@@ -216,7 +243,10 @@ GSD_redbyweek <- sapply(GSD, function(x) tapply(x$red, x$sample_week, sum, na.rm
 CSD_kingbyweek <- sapply(CSD, function(x) tapply(x$king, x$sample_week, sum, na.rm=T))
 GSD_kingbyweek <- sapply(GSD, function(x) tapply(x$king, x$sample_week, sum, na.rm=T))
 
-# finding quartiles to assign strata
+pooled_kingbyweek <- list()
+for(i in 1:4) pooled_kingbyweek[[i]] <- CSD_kingbyweek[[i]] + GSD_kingbyweek[[i]]
+
+# finding harvest quartiles (by date) to assign strata
 findquartile <- function(x) {
   xx <- cumsum(x)/sum(x)
   xx25 <- which.min(abs(xx-.25))
@@ -235,6 +265,7 @@ CSD_redbyweek_quartiles <- lapply(CSD_redbyweek, findquartile)
 GSD_redbyweek_quartiles <- lapply(GSD_redbyweek, findquartile)
 CSD_kingbyweek_quartiles <- lapply(CSD_kingbyweek, findquartile)
 GSD_kingbyweek_quartiles <- lapply(GSD_kingbyweek, findquartile)
+pooled_kingbyweek_quartiles <- lapply(pooled_kingbyweek, findquartile)
 
 # then assigning quartile-based strata to harvesting data.frames
 for(i in 1:4) {
@@ -242,6 +273,13 @@ for(i in 1:4) {
   CSD[[i]] <- CSD[[i]] %>% left_join(CSD_kingbyweek_quartiles[[i]]) %>% rename(strata_king=strata)
   GSD[[i]] <- GSD[[i]] %>% left_join(GSD_redbyweek_quartiles[[i]]) %>% rename(strata_red=strata)
   GSD[[i]] <- GSD[[i]] %>% left_join(GSD_kingbyweek_quartiles[[i]]) %>% rename(strata_king=strata)
+}
+# append GSD & CSD, assign quartile-based strata
+allSD <- list()
+for(i in 1:4) {
+  allSD[[i]] <- data.frame(sample_week=c(CSD[[i]]$sample_week, GSD[[i]]$sample_week),
+                           king=c(CSD[[i]]$king, GSD[[i]]$king))
+  allSD[[i]] <- allSD[[i]] %>% left_join(pooled_kingbyweek_quartiles[[i]]) %>% rename(strata_king_pooled=strata)
 }
 
 # assigning quartile-based strata to sampling data.frames ==> first separating by fishery
@@ -256,32 +294,33 @@ for(i in 1:4) {
   chinook_CSD[[i]] <- chinook_CSD[[i]] %>% left_join(CSD_kingbyweek_quartiles[[i]]) %>% rename(strata_king=strata)
   sockeye_GSD[[i]] <- sockeye_GSD[[i]] %>% left_join(GSD_redbyweek_quartiles[[i]]) %>% rename(strata_red=strata)
   chinook_GSD[[i]] <- chinook_GSD[[i]] %>% left_join(GSD_kingbyweek_quartiles[[i]]) %>% rename(strata_king=strata)
+  chinook[[i]] <- chinook[[i]] %>% left_join(pooled_kingbyweek_quartiles[[i]]) %>% rename(strata_king_pooled=strata)
 }
 
-# finally, actually testing proportions by strata!
-testthetab <- function(x) {
-  rawtab <- table(x$age, x$strata_red)
-  # print(rawtab)
-  bettertab <- rawtab[substr(rownames(rawtab),1,1) %in% 0:9, ]
-  # print(bettertab)
-  stillbettertab <- bettertab[rowSums(bettertab)>20,]
-  print(stillbettertab)
-  print(chisq.test(stillbettertab))
-}
-testthetab2 <- function(x) {
-  rawtab <- table(x$age, x$strata_king)
-  # print(rawtab)
-  bettertab <- rawtab[substr(rownames(rawtab),1,1) %in% 0:9, ]
-  # print(bettertab)
-  stillbettertab <- bettertab[rowSums(bettertab)>2,]
-  print(stillbettertab)
-  print(chisq.test(stillbettertab))
-}
-testthetab(x=sockeye_CSD[[1]])
-sapply(sockeye_CSD, testthetab)
-sapply(chinook_CSD, testthetab2)
-sapply(sockeye_GSD, testthetab)
-sapply(chinook_GSD, testthetab2)
+# # finally, actually testing proportions by strata!
+# testthetab <- function(x) {
+#   rawtab <- table(x$age, x$strata_red)
+#   # print(rawtab)
+#   bettertab <- rawtab[substr(rownames(rawtab),1,1) %in% 0:9, ]
+#   # print(bettertab)
+#   stillbettertab <- bettertab[rowSums(bettertab)>20,]
+#   print(stillbettertab)
+#   print(chisq.test(stillbettertab))
+# }
+# testthetab2 <- function(x) {
+#   rawtab <- table(x$age, x$strata_king)
+#   # print(rawtab)
+#   bettertab <- rawtab[substr(rownames(rawtab),1,1) %in% 0:9, ]
+#   # print(bettertab)
+#   stillbettertab <- bettertab[rowSums(bettertab)>2,]
+#   print(stillbettertab)
+#   print(chisq.test(stillbettertab))
+# }
+# testthetab(x=sockeye_CSD[[1]])
+# sapply(sockeye_CSD, testthetab)
+# sapply(chinook_CSD, testthetab2)
+# sapply(sockeye_GSD, testthetab)
+# sapply(chinook_GSD, testthetab2)
 
 
 # darn, will need to stratify proportions for sockeye
@@ -291,15 +330,16 @@ fixages <- function(x) {
   x1 <- x[substr(x$age,1,1) %in% 0:9, ]
   # x1$age <- paste(as.character(x1$age), sep=",",collapse="")
   # x1$age <- paste(as.character(x1$age), sep=".",collapse="")
-  x1$totage <- as.numeric(substr(x1$age,1,1)) + as.numeric(substr(x1$age,nchar(x1$age),nchar(x1$age)))
+  x1$totage <- 1 + as.numeric(substr(x1$age,1,1)) + as.numeric(substr(x1$age,nchar(x1$age),nchar(x1$age)))
   return(x1)
 }
 fixsex <- function(x) {
   x1 <- x[x$sex %in% c("F","M"), ]
-  x1$sex[x1$sex == "M"] <- " M"
+  x1$sex[x1$sex == "M"] <- " M"   # this is a hack to make M print before F for consistency with report
   return(x1)
 }
 
+## investigating values present, and testing fix functions
 for(i in 1:4) print(table(sockeye_CSD[[i]]$age))
 for(i in 1:4) print(table(sockeye_GSD[[i]]$age))
 for(i in 1:4) print(table(chinook_CSD[[i]]$age))
@@ -320,7 +360,8 @@ for(i in 1:4) print(table(fixsex(chinook_GSD[[i]])$sex))
 
 
 ## generalized stratified proportion function   ------ verify this thing!!!
-stratprop <- function(cat, strat, Nstrat) { # membership vec, strata vec, strata weights
+## ALSO -- include option for expanding total harvest
+stratprop <- function(cat, strat, Nstrat, Ntot=NULL) { # membership vec, strata vec, strata weights
   ntz <- table(strat, cat)
   nz <- colSums(ntz)
   nt <- rowSums(ntz)
@@ -331,18 +372,28 @@ stratprop <- function(cat, strat, Nstrat) { # membership vec, strata vec, strata
   vNtz <- (Nt^2)*vptz
   Nz <- colSums(Ntz)
   vNz <- colSums(vNtz)
-  pz <- Nz/sum(Nt)
-  vpz <- vNz/(sum(Nt)^2)
-  fortab <- paste0(round(100*pz, 2), "% (", round(100*sqrt(vpz), 2), "%)")
-  harv <- paste0(round(Nz, 0), " (", round(sqrt(vNz), 0), ")")
-  data.frame(nz,pz,vpz,fortab,harv)
+  if(is.null(Ntot)) Ntot <- sum(Nt)
+  pz <- Nz/Ntot   ## -- this would be where to redefine Ntot or similar
+  vpz <- vNz/(Ntot^2)
+  prop_for_tab <- paste0(round(100*pz, 2), "% (", round(100*sqrt(vpz), 2), "%)")
+  harv_for_tab <- paste0(round(Nz, 0), " (", round(sqrt(vNz), 0), ")")
+  data.frame(nz,pz,vpz,prop_for_tab,harv_for_tab)
 }
+
+## chi-squared test to see if stratification is necessary
 strattest <- function(cat, strat, Nstrat=NULL, printtab=F) { # membership vec, strata vec, strata weights
   ntz <- table(strat, cat)
   print(chisq.test(ntz))
   if(printtab) print(ntz)
 }
 
+## For each year, creating tables that are consistent with T8 & friends.
+## This happens by first creating tables by each combination of factors, then
+## splicing them together in the same order as in T8.
+
+## Following each spliced table is a sequence of chi-squared tests associated
+## with each table section.
+GSD_RED_ASL_update <- CSD_RED_ASL_update <- list()
 for(i in 1:4) {
   aa1 <- with(fixsex(fixages(sockeye_GSD[[i]])), 
               stratprop(cat=paste(totage,age,sex), 
@@ -366,7 +417,8 @@ for(i in 1:4) {
   
   aa12 <- rbind(aa1,aa2)
   aa12a <- aa12[order(rownames(aa12)), ]
-  print(rbind(aa12a, aa3, aa4))   ### this will become knitr::kable
+  GSD_RED_ASL_update[[i]] <- rbind(aa12a, aa3, aa4)
+  print(GSD_RED_ASL_update[[i]])   ### this will become knitr::kable
   with(fixsex(fixages(sockeye_GSD[[i]])), 
        strattest(cat=paste(totage,age,sex), 
                         strat=strata_red,  
@@ -407,7 +459,8 @@ for(i in 1:4) {
   
   aa12 <- rbind(aa1,aa2)
   aa12a <- aa12[order(rownames(aa12)), ]
-  print(rbind(aa12a, aa3, aa4))   ### this will become knitr::kable
+  CSD_RED_ASL_update[[i]] <- rbind(aa12a, aa3, aa4)
+  print(CSD_RED_ASL_update[[i]])   ### this will become knitr::kable
   with(fixsex(fixages(sockeye_CSD[[i]])), 
        strattest(cat=paste(totage,age,sex), 
                         strat=strata_red,  
@@ -426,34 +479,54 @@ for(i in 1:4) {
                         Nstrat=tapply(CSD[[i]]$red, CSD[[i]]$strata_red, sum, na.rm=T)))
 }
 
-### then figure out how to test &/or combine what was done for Chinook - pooled by both subdistricts!
-# creating pooled objects
-chinook_pooled <- list()
-for(i in 1:4) {
-  chinook_pooled[[i]] <- data.frame(age=c(chinook_GSD[[i]]$age, chinook_CSD[[i]]$age),
-                                    sex=c(chinook_GSD[[i]]$sex, chinook_CSD[[i]]$sex),
-                                    strata_king=c(chinook_GSD[[i]]$strata_king, chinook_CSD[[i]]$strata_king),
-                                    subdistrict=c(rep("GSD", nrow(chinook_GSD[[i]])), rep("CSD", nrow(chinook_CSD[[i]]))))
-}
+### Chinook ASL was pooled by both subdistricts in report!
+### need to test if this was appropriate
+# # creating pooled objects  -- might not be needed, just use chinook!
+# chinook_pooled <- list()
+# for(i in 1:4) {
+#   chinook_pooled[[i]] <- data.frame(age=c(chinook_GSD[[i]]$age, chinook_CSD[[i]]$age),
+#                                     sex=c(chinook_GSD[[i]]$sex, chinook_CSD[[i]]$sex),
+#                                     strata_king=c(chinook_GSD[[i]]$strata_king, chinook_CSD[[i]]$strata_king),
+#                                     subdistrict=c(rep("GSD", nrow(chinook_GSD[[i]])), rep("CSD", nrow(chinook_CSD[[i]]))))
+# }
+# 
+# ## testing if pooling by subdistrict is appropriate
+# for(i in 1:4) {
+#   cat("year",i,'\n')
+#   with(fixsex(fixages(chinook_pooled[[i]])),
+#        strattest(cat=paste(totage,sex), # was paste(totage,age,sex)
+#                  strat=subdistrict, printtab=T))
+#   with(fixsex(fixages(chinook_pooled[[i]])),
+#        strattest(cat=paste(totage), # was paste(totage,age)
+#                  strat=subdistrict, printtab=T)) 
+#   with(fixsex(fixages(chinook_pooled[[i]])),
+#        strattest(cat=paste(sex),
+#                  strat=subdistrict, printtab=T))  
+#   with(fixsex(chinook_pooled[[i]]),
+#        strattest(cat=paste(sex),
+#                  strat=subdistrict, printtab=T)) 
+# }
+# ## take-home: only evidence of difference by subdistrict in year 3 (2021) for first two tests (ages)
+# ## ==> idea: stratify by subdistrict?
 
 ## testing if pooling by subdistrict is appropriate
 for(i in 1:4) {
   cat("year",i,'\n')
-  with(fixsex(fixages(chinook_pooled[[i]])),
+  with(fixsex(fixages(chinook[[i]])),
        strattest(cat=paste(totage,sex), # was paste(totage,age,sex)
                  strat=subdistrict, printtab=T))
-  with(fixsex(fixages(chinook_pooled[[i]])),
+  with(fixsex(fixages(chinook[[i]])),
        strattest(cat=paste(totage), # was paste(totage,age)
                  strat=subdistrict, printtab=T)) 
-  with(fixsex(fixages(chinook_pooled[[i]])),
+  with(fixsex(fixages(chinook[[i]])),
        strattest(cat=paste(sex),
                  strat=subdistrict, printtab=T))  
-  with(fixsex(chinook_pooled[[i]]),
+  with(fixsex(chinook[[i]]),
        strattest(cat=paste(sex),
                  strat=subdistrict, printtab=T)) 
 }
 ## take-home: only evidence of difference by subdistrict in year 3 (2021) for first two tests (ages)
-## ==> idea: stratify by subdistrict!
+## ==> idea: stratify by subdistrict?
 
 ## testing if pooling by time strata is appropriate - GSD
 for(i in 1:4) {
@@ -493,19 +566,88 @@ for(i in 1:4) {   # - stratification breaks in year 2 (2020)
 }
 # take-home: no problems detected
 
-## testing if pooling by time strata is appropriate - pooled - not quite accurate but exploratory
+# ## testing if pooling by time strata is appropriate - pooled - not quite accurate but exploratory
+# for(i in 1:4) {   
+#   cat("year",i,'\n')
+#   with(fixsex(fixages(chinook_pooled[[i]])),
+#        strattest(cat=paste(totage,sex), # was paste(totage,age,sex)
+#                  strat=strata_king, printtab=T))
+#   with(fixsex(fixages(chinook_pooled[[i]])),
+#        strattest(cat=paste(totage), # was paste(totage,age)
+#                  strat=strata_king, printtab=T)) 
+#   with(fixsex(fixages(chinook_pooled[[i]])),
+#        strattest(cat=paste(sex),
+#                  strat=strata_king, printtab=T))  
+#   with(fixsex(chinook_pooled[[i]]),
+#        strattest(cat=paste(sex),
+#                  strat=strata_king, printtab=T)) 
+# }
+
+## testing if pooling by time strata is appropriate 
 for(i in 1:4) {   
   cat("year",i,'\n')
-  with(fixsex(fixages(chinook_pooled[[i]])),
+  with(fixsex(fixages(chinook[[i]])),
        strattest(cat=paste(totage,sex), # was paste(totage,age,sex)
-                 strat=strata_king, printtab=T))
-  with(fixsex(fixages(chinook_pooled[[i]])),
+                 strat=strata_king_pooled, printtab=T))
+  with(fixsex(fixages(chinook[[i]])),
        strattest(cat=paste(totage), # was paste(totage,age)
-                 strat=strata_king, printtab=T)) 
-  with(fixsex(fixages(chinook_pooled[[i]])),
+                 strat=strata_king_pooled, printtab=T)) 
+  with(fixsex(fixages(chinook[[i]])),
        strattest(cat=paste(sex),
-                 strat=strata_king, printtab=T))  
-  with(fixsex(chinook_pooled[[i]]),
+                 strat=strata_king_pooled, printtab=T))  
+  with(fixsex(chinook[[i]]),
        strattest(cat=paste(sex),
-                 strat=strata_king, printtab=T)) 
+                 strat=strata_king_pooled, printtab=T)) 
+}
+## take-home: problems in years 2 and 3 - maybe stratify temporally throughout??
+
+
+## For each year, creating tables that are consistent with T8 & friends.
+## This happens by first creating tables by each combination of factors, then
+## splicing them together in the same order as in T8.
+
+## Following each spliced table is a sequence of chi-squared tests associated
+## with each table section.
+KING_ASL_update <- list()
+for(i in 1:4) {
+  aa1 <- with(fixsex(fixages(chinook[[i]])), 
+              stratprop(cat=paste(totage,age,sex), 
+                        strat=strata_king_pooled,  
+                        Nstrat=tapply(allSD[[i]]$king, allSD[[i]]$strata_king_pooled, sum, na.rm=T)))
+  
+  aa2 <- with(fixsex(fixages(chinook[[i]])), 
+              stratprop(cat=paste(totage,age,"total"), 
+                        strat=strata_king_pooled,  
+                        Nstrat=tapply(allSD[[i]]$king, allSD[[i]]$strata_king_pooled, sum, na.rm=T)))
+  
+  aa3 <- with(fixsex(fixages(chinook[[i]])), 
+              stratprop(cat=paste(sex), 
+                        strat=strata_king_pooled,  
+                        Nstrat=tapply(allSD[[i]]$king, allSD[[i]]$strata_king_pooled, sum, na.rm=T)))
+  
+  aa4 <- with(fixsex(chinook[[i]]), 
+              stratprop(cat=paste(sex), 
+                        strat=strata_king_pooled,  
+                        Nstrat=tapply(allSD[[i]]$king, allSD[[i]]$strata_king_pooled, sum, na.rm=T)))
+  
+  aa12 <- rbind(aa1,aa2)
+  aa12a <- aa12[order(rownames(aa12)), ]
+  KING_ASL_update[[i]] <- rbind(aa12a, aa3, aa4)
+  print(KING_ASL_update[[i]])   ### this will become knitr::kable
+  with(fixsex(fixages(chinook[[i]])), 
+       strattest(cat=paste(totage,age,sex), 
+                 strat=strata_king_pooled,  
+                 Nstrat=NULL))
+  with(fixsex(fixages(chinook[[i]])), 
+       strattest(cat=paste(totage,age,"total"), 
+                 strat=strata_king_pooled,  
+                 Nstrat=NULL))
+  with(fixsex(fixages(chinook[[i]])), 
+       strattest(cat=paste(sex), 
+                 strat=strata_king_pooled,  
+                 Nstrat=NULL))
+  with(fixsex(chinook[[i]]), 
+       strattest(cat=paste(sex), 
+                 strat=strata_king_pooled,  
+                 Nstrat=NULL))
 }
